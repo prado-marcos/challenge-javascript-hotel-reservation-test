0b8af680bad765b4ed511852dc799ae9013c0bb8
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-05-09 08:18:01 -0300

    [feat]: implemented getCheapestHotel function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,40 @@
-function getCheapestHotel (input) { //DO NOT change the function's name.
-    return "Cheapest hotel name"
+const { Lakewood, Bridgewood, Ridgewood } = require("./hotel");
+const {
+    getClientType,
+    bookingDates,
+    getHotelPrices,
+    getAllCheapest,
+} = require("./util");
+
+function getCheapestHotel(input) {
+    try {
+        const hotels = [Lakewood, Bridgewood, Ridgewood];
+        const clientType = getClientType(input);
+        const dates = bookingDates(input);
+        const hotelPrices = getHotelPrices(hotels, dates, clientType);
+        const cheapestHotel = Math.min(...hotelPrices);
+        const cheapestsIndexes = getAllCheapest(hotelPrices, cheapestHotel);
+        const indexOfCheapestPrice = cheapestsIndexes.slice(-1);
+        return hotels[indexOfCheapestPrice].name;
+    } catch (err) {
+        return err.message;
+    }
 }
 
-exports.getCheapestHotel = getCheapestHotel
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question(
+    `
+    Formato da entrada: <tipo_do_cliente>: <data1>, <data2>, <data3>, …
+    Exemplo: Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)
+    `,
+    (input) => {
+        console.log(getCheapestHotel(input));
+        readline.close();
+    }
+);
+
+exports.getCheapestHotel = getCheapestHotel;
